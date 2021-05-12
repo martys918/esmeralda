c7b4db003f6ba9983ad82dbd4f8b67c741912b22
marty-s-miyake <marty.s.miyake@gmail.com>
2021-05-12 16:08:10 +0900

    Add Request reviewers with the added label(github)

---
+++ index.js
@@ -0,0 +1,47 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const fs = require('fs')
+
+const pull_request = () => {
+  return github.context.payload.pull_request
+}
+
+const labeled = () => {
+  return github.context.payload.label
+}
+
+const pull_request_author = () => {
+  return github.context.payload.pull_request.user.login
+}
+
+const request_reviewers = () => {
+  const path = core.getInput('request_reviewers')
+  return JSON.parse(fs.readFileSync(path, 'utf8'))[labeled().name]
+}
+
+const repository = () => {
+  return {
+    owner: github.context.repo.owner,
+    name: github.context.repo.repo,
+  }
+}
+
+const run = async () => {
+  try {
+    const token = core.getInput('github-token')
+    const octokit = github.getOctokit(token)
+
+    await octokit.pulls.requestReviewers({
+      owner: repository().owner,
+      repo: repository().name,
+      pull_number: pull_request().number,
+      reviewers: request_reviewers().filter(n => n !== pull_request_author())
+    })
+  }
+  catch (error) {
+    core.error(error)
+    core.setFailed(error.message)
+  }
+}
+
+run()
